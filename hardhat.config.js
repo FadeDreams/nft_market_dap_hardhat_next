e46c70da867a059b76d3ad23cc1dac685731f19f
fadedreams7 <fadedreams7@gmail.com>
2024-05-05 07:01:01 +0000

    config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
-};
+  solidity: "0.8.9",
+  networks:{
+    hardhat:{
+
+    },
+    sepolia:{
+      url: process.env.INFURA_URL,
+      accounts:[process.env.WALLET_PRIVATE_KEY]
+    }
+  }
+}
